3b1a7491ba55766d863604e84fb550474655314e
TomBoBotSer <tim.m.kloss@gmail.com>
2024-11-15 16:05:01 +0100

    added docstrings

--- src/smallangle/smallangle.py
+++ src/smallangle/smallangle.py
@@ -3,17 +3,25 @@ import numpy as np
 from numpy import pi
 import pandas as pd
 
-@click.group()
+CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
+
+@click.group(context_settings=CONTEXT_SETTINGS)
 def geef_2_plz():
+    """This script calculates either the sin or the tan of given x-values.
+    """
     pass
 
 @geef_2_plz.command()
 @click.option(
     "-n",
     "--number",
-    default=1,
+    help = "The number (int) represents the amount of even parts into which you want the interval to be divided.",
+
+    default=10,
 )
 def sin(number):
+    """Calculates the sin of the given x-value.
+    """
     x = np.linspace(0, 2 * pi, number)
     df = pd.DataFrame({"x": x, "sin (x)": np.sin(x)})
     print(df)
@@ -22,9 +30,13 @@ def sin(number):
 @click.option(
     "-n",
     "--number",
-    default=1,
+    help = "The number (int) represents the amount of even parts into which you want the interval to be divided.",
+    
+    default=10,
 )
 def tan(number):
+    """Calculates the tan of the given x-value.
+    """
     x = np.linspace(0, 2 * pi, number)
     df = pd.DataFrame({"x": x, "tan (x)": np.tan(x)})
     print(df)
