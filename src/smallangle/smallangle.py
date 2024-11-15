89123eb2a6e41a70649cc4deeb8e63bce4714820
TomBoBotSer <tim.m.kloss@gmail.com>
2024-11-15 15:32:54 +0100

    click commands toegevoegd

--- src/smallangle/smallangle.py
+++ src/smallangle/smallangle.py
@@ -3,13 +3,27 @@ import numpy as np
 from numpy import pi
 import pandas as pd
 
+@click.group()
+def geef_2_plz():
+    pass
 
+@geef_2_plz.command()
+@click.option(
+    "-n",
+    "--number",
+    default=1,
+)
 def sin(number):
     x = np.linspace(0, 2 * pi, number)
     df = pd.DataFrame({"x": x, "sin (x)": np.sin(x)})
     print(df)
 
-
+@geef_2_plz.command()
+@click.option(
+    "-n",
+    "--number",
+    default=1,
+)
 def tan(number):
     x = np.linspace(0, 2 * pi, number)
     df = pd.DataFrame({"x": x, "tan (x)": np.tan(x)})
@@ -17,4 +31,4 @@ def tan(number):
 
 
 if __name__ == "__main__":
-    sin(10)
+    geef_2_plz()
